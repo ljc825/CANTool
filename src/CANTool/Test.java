af2dacaa03ad884349095b2d2ff86f8f97bffe5d
ljc825 <413455281@qq.com>
2017-09-29 18:02:47 +0800

    add new test case for CANTOOLAPP

--- src/CANTool/Test.java
+++ src/CANTool/Test.java
@@ -20,15 +20,17 @@ public class Test {
 		InputStream in2 = new FileInputStream(new File("test2.txt"));
 		InputStream in3 = new FileInputStream(new File("test3.txt"));
 		InputStream in4 = new FileInputStream(new File("test4.txt"));
+		InputStream in5 = new FileInputStream(new File("test5.txt"));
 		Scanner scan = new Scanner(System.in);
 		Scanner scan1 = new Scanner(in1);
 		Scanner scan2 = new Scanner(in2);
 		Scanner scan3 = new Scanner(in3);
 		Scanner scan4 = new Scanner(in4);
+		Scanner scan5 = new Scanner(in5);
 		Thread thread = new Thread();
 		String tempString;
 		int time;
-		//1固定时间间隔，2随机时间间隔，3快速，4单信息测试曲线
+		//1固定时间间隔，2随机时间间隔，3快速，4单信息测试曲线,多格式数据测试
 		while(scan.hasNext())
 		{
 			int temp = scan.nextInt();
@@ -67,6 +69,15 @@ public class Test {
 					tempString=scan4.next()+"\r";
 					SerialTool.sendToPort(serialPort, tempString.getBytes());
 					thread.sleep(50);
+				}
+			}
+			else if(temp==5)
+			{
+				for(int i=0;i<1000;i++)
+				{
+					tempString=scan5.next()+"\r";
+					SerialTool.sendToPort(serialPort, tempString.getBytes());
+					thread.sleep(100);
 				}
 			}
 		}
